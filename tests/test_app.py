8c4ff2dd5eb1e50ff0df618ec70213335d3cb856
STSS <stss@lundbeck.com>
2024-10-21 12:03:27 +0200

    Updated test

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,8 +12,7 @@ class BasicTests(unittest.TestCase):
 	    "Code is like humor. When you have to explain it, it’s bad. - Cory House",
 	    "Fix the cause, not the symptom. - Steve Maguire",
 	    "Optimism is an occupational hazard of programming.- Kent Beck",
-	    "Simplicity is the soul of efficiency. - Austin Freeman"
-        ]
+	    "Simplicity is the soul of efficiency. - Austin Freeman"]
 
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
